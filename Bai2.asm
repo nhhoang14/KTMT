92ff2cf1ab691bc3e6a1a4ed8b52e03381e0f2e4
nhhoang.14 <nhhoangne@gmail.com>
2025-05-19 11:21:17 +0700

    Add files via upload

---
+++ Bai2.asm
@@ -0,0 +1,21 @@
+.Model Small
+.Stack 100h
+.Code
+MAIN Proc
+    MOV AH, 1
+    INT 21H
+    MOV BL, AL
+    
+    MOV AH, 2
+    MOV DL, 0DH
+    INT 21H
+    
+    MOV DL, 0AH
+    INT 21H
+    
+    MOV DL, BL
+    INT 21H
+    
+    MOV AH, 4CH
+    INT 21H
+MAIN Endp
