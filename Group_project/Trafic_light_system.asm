e42f5b0dd2de85aec581c0466c262b234c324cf8
nhhoang.14 <nhhoangne@gmail.com>
2025-05-26 17:47:02 +0700

    Update Trafic_light_system.asm

--- Group_project/Trafic_light_system.asm
+++ Group_project/Trafic_light_system.asm
@@ -2,41 +2,38 @@
 
 name "traffic"
 
-mov ax, all_red
-out 4, ax
+MOV AX, all_red
+OUT 4, AX
 
-mov si, offset situation
+MOV SI, offset situation
 
 next:
-mov ax, [si]
-out 4, ax
+MOV AX, [SI]
+OUT 4, AX
 
 ; wait 1 seconds (1 million microseconds)
-mov     cx, 0FH    ;    004C4B40h = 5,000,000
-mov     dx, 4240H
-mov     ah, 86h
-int     15h   
+MOV cx, 0FH ; 004C4B40h = 5,000,000
+MOV dx, 4240H
+MOV ah, 86H
+INT 15H   
 
-mov     cx, 0FH    ;    004C4B40h = 5,000,000
-mov     dx, 4240H
-mov     ah, 86h
-int     15h
+MOV cx, 0FH ; 004C4B40h = 5,000,000
+MOV dx, 4240H
+MOV ah, 86H
+INT 15H
 
-add si, 2 ; next situation
-cmp si, sit_end
-jb  next
-mov si, offset situation
-jmp next
+ADD SI, 2   ; next situation
+CMP SI, sit_end
+JB  next
+MOV SI, offset situation
+JMP next
 
-;                        FEDC_BA98_7654_3210
-situation        dw      0000_0011_0000_1100b
-s1               dw      0000_0110_1001_1010b
-s2               dw      0000_1000_0110_0001b
-s3               dw      0000_1000_0110_0001b
-s4               dw      0000_0100_1101_0011b
+; FEDC_BA98_7654_3210
+situation dw 0000_0011_0000_1100b
+s1 dw 0000_0110_1001_1010b
+s2 dw 0000_1000_0110_0001b
+s3 dw 0000_1000_0110_0001b
+s4 dw 0000_0100_1101_0011b
 sit_end = $
 
-all_red          equ     0000_0010_0100_1001b
-
-
-
+all_red equ 0000_0010_0100_1001b
