bb9fb68ac811d5eb9cd5d5467a67b322d28f2681
nhhoang.14 <nhhoangne@gmail.com>
2025-05-27 18:29:13 +0700

    Update Bai10.asm

--- Individual_project/Bai10.asm
+++ Individual_project/Bai10.asm
@@ -1,48 +1,60 @@
-.Model Small
-.Stack 100H
-.Data
-    TB1 DB 'Chuoi ban dau la: Kien truc may tinh'  ; Khai bao TB1
-    TB2 DB 13,10,'Tong chieu dai cua chuoi: $'  ; Khai bao TB2
-    S   DB 'Kien truc may tinh$' ; Chuoi can dem
-.Code
-    MAIN Proc
+.MODEL SMALL
+.STACK 100H
+.DATA
+    TB1 DB 'Chuoi ban dau la: Kien truc may tinh$'
+    TB2 DB 13,10,'Tong chieu dai cua chuoi: $'
+    S   DB 'Kien truc may tinh$'
+.CODE
+    MAIN PROC
         ; Khoi tao cho thanh ghi DS
         MOV AX, @DATA
         MOV DS, AX
-        
+    
         ; In ra man hinh xau TB1
         LEA DX, TB1
-        MOV AH, 9  
+        MOV AH, 9
         INT 21H
         
+        ; Tinh chieu dai chuoi S (dem so ky tu truoc dau $)
+        LEA SI, S
+        MOV CX, 0   ; CX se dem do dai
+    
+    CountLoop:
+        MOV AL, [SI]
+        CMP AL, '$' ; Neu gap ky tu ket thuc thi dung
+        JE DoneCounting
+        INC CX
+        INC SI
+        JMP CountLoop
+    
+    DoneCounting:
         ; In ra man hinh xau TB2
         LEA DX, TB2
         MOV AH, 9
         INT 21H
-        
-        ; Tinh chieu dau chuoi
-        XOR AX, AX
-        MOV AL, S+1 ; Chuyen chieu dai chuoi vao AX 
-        MOV CX, 0   ; Khoi tao bien dem (count)
-        MOV BX, 10  ; Khoi tao so chia
-        
-    PushLoop:    ; Day cac so vao ngan xep
-        MOV DX, 0   ; Khoi tao phan du bang 0
-        DIV BX  ; Chia 10
-        PUSH DX ; Lay DX la phan du day vao ngan xep
-        INC CX  ; Tang CX len 1 don vi
-        CMP AX, 0   ; So sanh AX phan thuong (khac 0 thi tiep tuc vong lap)
-        JNZ PushLoop ; Jump not zero  
-        
-    PrintLoop:    ; Lay cac so tu ngan xep ra
-        POP DX  ; Chua so du trong phep chia  
-        ADD DX, '0' ; Chuyen chu so -> ky tu so
+    
+        ; In so ky tu (CX chua do dai chuoi)
+        MOV AX, CX
+        MOV BX, 10
+        XOR CX, CX
+    
+    PushLoop:
+        XOR DX, DX
+        DIV BX
+        PUSH DX
+        INC CX
+        CMP AX, 0
+        JNZ PushLoop
+    
+    PrintLoop:
+        POP DX
+        ADD DL, '0'
         MOV AH, 2
         INT 21H
         LOOP PrintLoop
-        
-        ; Ket thuc tro ve chuong trinh goi
+    
+        ; Ket thuc chuong trinh
         MOV AH, 4CH
         INT 21H
-    MAIN Endp
+    MAIN ENDP
 END MAIN
