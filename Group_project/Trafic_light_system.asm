168832e1e56164d487a7d1b5daada40b68e90758
nhhoang.14 <nhhoangne@gmail.com>
2025-05-27 16:45:25 +0700

    Update Trafic_light_system.asm

--- Group_project/Trafic_light_system.asm
+++ Group_project/Trafic_light_system.asm
@@ -5,11 +5,12 @@ name "traffic"
 .Stack 100h
 .Data
     ; FEDC_BA98_7654_3210
-    situation dw 0000_0011_0000_1100b
-    s1 dw 0000_0110_1001_1010b
-    s2 dw 0000_1000_0110_0001b
-    s3 dw 0000_1000_0110_0001b
-    s4 dw 0000_0100_1101_0011b
+    situation dw 0000_0011_0000_1100b ; Xanh doc  
+    s1 dw 0000_0011_0000_1100b ; Xanh doc
+    s2 dw 0000_0010_1000_1010b  ; Vang doc
+    s3 dw 0000_1000_0110_0001b  ; Xanh ngang
+    s4 dw 0000_1000_0110_0001b  ; Xang ngang
+    s5 dw 0000_0100_0101_0001b  ; Vang ngang
     sit_end = $
 
     all_red equ 0000_0010_0100_1001b
