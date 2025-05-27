f11e3ca5ac7672d46f32500fe3a221e3fac4e2a9
nhhoang.14 <nhhoangne@gmail.com>
2025-05-27 14:52:16 +0700

    Update Trafic_light_system.asm

--- Group_project/Trafic_light_system.asm
+++ Group_project/Trafic_light_system.asm
@@ -1,38 +1,46 @@
 #start=Traffic_Lights.exe#
 
 name "traffic"
+.Model Small
+.Stack 100h
+.Data
+    ; FEDC_BA98_7654_3210
+    situation dw 0000_0011_0000_1100b
+    s1 dw 0000_0110_1001_1010b
+    s2 dw 0000_1000_0110_0001b
+    s3 dw 0000_1000_0110_0001b
+    s4 dw 0000_0100_1101_0011b
+    sit_end = $
 
-MOV AX, all_red
-OUT 4, AX
-
-MOV SI, offset situation
+    all_red equ 0000_0010_0100_1001b
+.Code
+MAIN Proc
+    MOV AX, @DATA
+    MOV DS, AX
+    
+    MOV AX, all_red
+    OUT 4, AX
+    
+    MOV SI, offset situation
 
 next:
-MOV AX, [SI]
-OUT 4, AX
+    MOV AX, [SI]
+    OUT 4, AX
 
-MOV cx, 0FH ; 004C4B40h = 5,000,000
-MOV dx, 4240H
-MOV ah, 86H
-INT 15H   
-
-MOV cx, 0FH ; 004C4B40h = 5,000,000
-MOV dx, 4240H
-MOV ah, 86H
-INT 15H
-
-ADD SI, 2   ; next situation
-CMP SI, sit_end
-JB  next
-MOV SI, offset situation
-JMP next
-
-; FEDC_BA98_7654_3210
-situation dw 0000_0011_0000_1100b
-s1 dw 0000_0110_1001_1010b
-s2 dw 0000_1000_0110_0001b
-s3 dw 0000_1000_0110_0001b
-s4 dw 0000_0100_1101_0011b
-sit_end = $
-
-all_red equ 0000_0010_0100_1001b
+    MOV cx, 0FH ; 004C4B40h = 5,000,000
+    MOV dx, 4240H
+    MOV ah, 86H
+    INT 15H   
+    
+    MOV cx, 0FH ; 004C4B40h = 5,000,000
+    MOV dx, 4240H
+    MOV ah, 86H
+    INT 15H
+    
+    ADD SI, 2   ; next situation
+    CMP SI, sit_end
+    JB  next
+    MOV SI, offset situation
+    JMP next
+MAIN Endp
+End MAIN
