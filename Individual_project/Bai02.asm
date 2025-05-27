4b44af3eada3bbf409d194123b18ad7f0d1ec40b
nhhoang.14 <nhhoangne@gmail.com>
2025-05-27 18:02:19 +0700

    Update Bai02.asm

--- Individual_project/Bai02.asm
+++ Individual_project/Bai02.asm
@@ -1,37 +1,37 @@
-.Model Small
-.Stack 100H
-.Data
-    TB1 DB 'Hay nhap mot ky tu: $'  ; Khai bao TB1
-    TB2 DB 13,10,'Ky tu da nhap: $' ; Khai bao TB2
-    OUTPUT DB ? ; Khai bao bien Kytu khong co gia tri ban dau
-.Code
-    MAIN Proc
-        ; Khoi tao cho thanh ghi DS
-        MOV AX, @DATA   ; Nap dia chi doan du lieu vao AX
-        MOV DS, AX  ; Nap dia chi doan du lieu vao DS
+.MODEL SMALL
+.STACK 100H
+.DATA
+    TB1 DB 'Hay nhap mot ky tu: $'      ; Khai bao TB1
+    TB2 DB 13,10,'Ky tu da nhap: $'     ; Khai bao TB2
+    OUTPUT DB ?                         ; Khai bao bien KyTu chua co gia tri
+.CODE
+    MAIN PROC
+        ; Khoi tao thanh ghi DS
+        MOV AX, @DATA
+        MOV DS, AX
     
         ; In ra man hinh xau TB1
-        LEA DX, TB1 ; Nap dia chi TB1 vao DX
-        MOV AH, 9   ; Dung ham ngat 9 cua ngat 21H
+        LEA DX, TB1
+        MOV AH, 9
         INT 21H
     
-        ; Nhap vao 1 ki tu tu ban phim
-        MOV AH, 1   ; Dung ham ngat 1 cua ngat 21H
+        ; Nhap 1 ky tu tu ban phim
+        MOV AH, 1
         INT 21H
-        MOV OUTPUT, AL  ; Nap gia tri vua nhap (duoc luu o AL) vao bien KyTu
+        MOV OUTPUT, AL
     
         ; In ra man hinh xau TB2
-        LEA DX, TB2 ; Nap dia chi TB2 vao DX
-        MOV AH, 9   ; Dung ham ngat 9 cua ngat 21H
+        LEA DX, TB2
+        MOV AH, 9
         INT 21H
     
-        ; Hien thi ky tu da nhap
-        MOV AH, 2   ; Su dung ham ngat 2 cua ngat 21h
-        MOV DL, OUTPUT  ; Hien thi ra man hinh ky tu da luu o DL
+        ; Hien thi ky tu vua nhap
+        MOV AH, 2
+        MOV DL, OUTPUT
         INT 21H
     
-        ; Ket thuc tro ve chuong trinh goi
-        MOV AH, 4CH ; Dung ham 4CH cua ngat 21H
+        ; Ket thuc chuong trinh
+        MOV AH, 4CH
         INT 21H
-    MAIN Endp
+    MAIN ENDP
 END MAIN
